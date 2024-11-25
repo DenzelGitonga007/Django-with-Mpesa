539edeb4c3cf20d63e98c3a602d69766ff50db02
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-11-25 13:35:48 +0300

    urls

--- medi_app/urls.py
+++ medi_app/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('appointment/', views.appointment, name="appointment"), # appointment url
     path('show_appointments/', views.retrieve_appointments, name="show_appointments"), # url to show the appoinments
     path('delete/<int:id>', views.delete_appointment, name="delete_appointment"), # delete
-    path('edit/<int:appointment_id>', views.update_appointment, name="update_appointment")
+    path('edit/<int:appointment_id>', views.update_appointment, name="update_appointment"),
+
+    path('pay/', views.pay, name='pay'), # view the payment form
+    path('stk/', views.stk, name='stk'), # send the stk push prompt
+    path('token/', views.token, name='token'), # generate the token for that particular transaction
 ]
